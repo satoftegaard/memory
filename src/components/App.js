d5e386931cffaa54edff762ed9236f83bd09dd90
sara toftegaard <satoftegaard@gmail.com>
2016-12-11 19:21:21 -0500

    explorer mode done

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,14 @@ const images = [
   require('../images/h-5.jpg'),
   require('../images/h-6.jpg'),
   require('../images/h-7.jpg'),
+  require('../images/h-8.jpg'),
+  require('../images/h-1.jpg'),
+  require('../images/h-2.jpg'),
+  require('../images/h-3.jpg'),
+  require('../images/h-4.jpg'),
+  require('../images/h-5.jpg'),
+  require('../images/h-6.jpg'),
+  require('../images/h-7.jpg'),
   require('../images/h-8.jpg')
 ]
 
@@ -24,8 +32,10 @@ class App extends Component {
     }
   }
 
+  // choose card
+
   choose (card) {
-    if (this.state.picks.length === 2 || this.state.matched.includes(card)) { return } // DO NOTHING, BAIL OUT
+    if (this.state.picks.length === 2 || this.state.matched.includes(card)) { return }
     this.setState({
       picks: [...this.state.picks, card]
     }, () => {
@@ -74,16 +84,16 @@ class App extends Component {
             <BoxItem image={images[7]} isUp={picks.includes(7) || matched.includes(7)} handleClick={() => this.choose(7)} />
           </tr>
           <tr>
-            <BoxItem image={images[0]} isUp={picks.includes(0) || matched.includes(0)} handleClick={() => this.choose(0)} />
-            <BoxItem image={images[1]} isUp={picks.includes(1) || matched.includes(1)} handleClick={() => this.choose(1)} />
-            <BoxItem image={images[2]} isUp={picks.includes(2) || matched.includes(2)} handleClick={() => this.choose(2)} />
-            <BoxItem image={images[3]} isUp={picks.includes(3) || matched.includes(3)} handleClick={() => this.choose(3)} />
+            <BoxItem image={images[8]} isUp={picks.includes(8) || matched.includes(8)} handleClick={() => this.choose(8)} />
+            <BoxItem image={images[9]} isUp={picks.includes(9) || matched.includes(9)} handleClick={() => this.choose(9)} />
+            <BoxItem image={images[10]} isUp={picks.includes(10) || matched.includes(10)} handleClick={() => this.choose(10)} />
+            <BoxItem image={images[11]} isUp={picks.includes(11) || matched.includes(11)} handleClick={() => this.choose(11)} />
           </tr>
           <tr>
-            <BoxItem image={images[4]} isUp={picks.includes(4) || matched.includes(4)} handleClick={() => this.choose(4)} />
-            <BoxItem image={images[5]} isUp={picks.includes(5) || matched.includes(5)} handleClick={() => this.choose(5)} />
-            <BoxItem image={images[6]} isUp={picks.includes(6) || matched.includes(6)} handleClick={() => this.choose(6)} />
-            <BoxItem image={images[7]} isUp={picks.includes(7) || matched.includes(7)} handleClick={() => this.choose(7)} />
+            <BoxItem image={images[12]} isUp={picks.includes(12) || matched.includes(12)} handleClick={() => this.choose(12)} />
+            <BoxItem image={images[13]} isUp={picks.includes(13) || matched.includes(13)} handleClick={() => this.choose(13)} />
+            <BoxItem image={images[14]} isUp={picks.includes(14) || matched.includes(14)} handleClick={() => this.choose(14)} />
+            <BoxItem image={images[15]} isUp={picks.includes(15) || matched.includes(15)} handleClick={() => this.choose(15)} />
           </tr>
         </tbody>
       </table>
